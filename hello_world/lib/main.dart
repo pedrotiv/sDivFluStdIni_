9f1c91e92519d71766e78775b3eb284e2d521723
edu4u-games <59312731+edu4u-games@users.noreply.github.com>
2020-03-05 12:22:47 -0300

    :feat: hello_word: add code at line 30

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
+// The start point function for all Flutter App (=> is a sugar for simple function return)
+// runApp() inflates the given widget and attach it to the screen and creates a 
+// (new) instance of MyApp Widget class
 void main() => runApp(MyApp());
 
+// All in Flutter is a Widget, the StatelessWidget is a widget class that does not require mutable state.
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your application, the abstract class Widget 
+  // describes the configuration for an [Element], where BuildContext class is a
+  //  handle to the location of a widget in the widget tree.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Hello World',
+      theme: ThemeData(
+       primarySwatch: Colors.red,
+        ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),          
+          title: Text('Welcome to Flutter',),          
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Text('Hello World', textScaleFactor: 4.0, ),
         ),
       ),
     );
